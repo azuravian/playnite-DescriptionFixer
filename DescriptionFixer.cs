4118f5e24e8c6788dd1e28c899acd18ace4b05de
Gregory Booth <azuravian@gmail.com>
2025-08-10 18:06:30 -0400

    Add project files.

---
+++ DescriptionFixer.cs
@@ -0,0 +1,81 @@
+﻿using Playnite.SDK;
+using Playnite.SDK.Events;
+using Playnite.SDK.Models;
+using Playnite.SDK.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace DescriptionFixer
+{
+    public class DescriptionFixer : GenericPlugin
+    {
+        private static readonly ILogger logger = LogManager.GetLogger();
+
+        private DescriptionFixerSettingsViewModel settings { get; set; }
+
+        public override Guid Id { get; } = Guid.Parse("b86cf867-0519-49ef-b7b7-386b6a1e2830");
+
+        public DescriptionFixer(IPlayniteAPI api) : base(api)
+        {
+            settings = new DescriptionFixerSettingsViewModel(this);
+            Properties = new GenericPluginProperties
+            {
+                HasSettings = true
+            };
+        }
+
+        public override void OnGameInstalled(OnGameInstalledEventArgs args)
+        {
+            // Add code to be executed when game is finished installing.
+        }
+
+        public override void OnGameStarted(OnGameStartedEventArgs args)
+        {
+            // Add code to be executed when game is started running.
+        }
+
+        public override void OnGameStarting(OnGameStartingEventArgs args)
+        {
+            // Add code to be executed when game is preparing to be started.
+        }
+
+        public override void OnGameStopped(OnGameStoppedEventArgs args)
+        {
+            // Add code to be executed when game is preparing to be started.
+        }
+
+        public override void OnGameUninstalled(OnGameUninstalledEventArgs args)
+        {
+            // Add code to be executed when game is uninstalled.
+        }
+
+        public override void OnApplicationStarted(OnApplicationStartedEventArgs args)
+        {
+            // Add code to be executed when Playnite is initialized.
+        }
+
+        public override void OnApplicationStopped(OnApplicationStoppedEventArgs args)
+        {
+            // Add code to be executed when Playnite is shutting down.
+        }
+
+        public override void OnLibraryUpdated(OnLibraryUpdatedEventArgs args)
+        {
+            // Add code to be executed when library is updated.
+        }
+
+        public override ISettings GetSettings(bool firstRunSettings)
+        {
+            return settings;
+        }
+
+        public override UserControl GetSettingsView(bool firstRunSettings)
+        {
+            return new DescriptionFixerSettingsView();
+        }
+    }
+}
